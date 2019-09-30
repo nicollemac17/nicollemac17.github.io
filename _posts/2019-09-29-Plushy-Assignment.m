785824d72606827b317b37dd7261c998ff261cc7
nicollemac17 <54951170+nicollemac17@users.noreply.github.com>
2019-09-29 22:56:11 -0400

    Create 2019-09-29-Plushy-Assignment.m

---
+++ _posts/2019-09-29-Plushy-Assignment.m
@@ -0,0 +1,32 @@
+---
+layout: post
+title: Plushy Assignment!  
+subtitle: Homework Assignment 4 - Due 10/01/2019
+tags: [homework, assignments, sewing, coding]
+comments: true
+---
+
+## Assignment: 
+Our assignment for today was to create a light up plushy. First, I had to do a paper prototype of the plushy to make sure that the way the project was being set up was using the resources in the best possible way. Next, I had to try take the paper prototype and apply it to a more concrete way to make sure it works. To do this, you have to use alligator clips along with lilypad LEDs, battery holders, a slide switch, and a button. Once this works, you then begin to apply the mechanisms to the felt paper and secure it by using conductive thread. Finally, you perform some plushy surgery and put the plushy together to make a light-up stuffed animal!
+
+## Look for yourself!
+
+![The Front - 1](https://nicollemac17.github.io/img/Screen Shot 2019-09-25 at 4.03.45 PM.png)
+
+![The Back - 2](https://nicollemac17.github.io/img/Screen Shot 2019-09-25 at 4.04.22 PM.png)
+
+![Alligator Prototyping - 3](https://nicollemac17.github.io/img/Screen Shot 2019-09-25 at 4.04.54 PM.png)
+
+![Plushy Paper Prototype - 4](https://nicollemac17.github.io/img/Screen Shot 2019-09-25 at 4.07.13 PM.png)
+
+I'm excited with the outcome of the project! 
+
+## Some advice I would give my past self:
+
+Stay tuned for some more exciting coding adventures! Until next time! 
+
+XOXO, 
+
+Coding Girl
+
+
